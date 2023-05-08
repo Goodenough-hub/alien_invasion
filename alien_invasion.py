c8916fbb77d9a402cba164c8408e448b7590dd31
Goodenough <2323342501@qq.com>
2023-05-08 17:32:55 +0800

    add a swarm of alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,12 +29,32 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """创建外星人群。"""
-        # 创建一个外星人
+        # 创建一个外星人并计算一行可容纳多少个外星人。
+        # 外星人的间距为外星人宽度。
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
 
-        # 设置背景色
-        self.bg_color = (230, 230, 230)  # 浅灰色
+        # 计算屏幕可容纳多少行外星人。
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # 创建外星人群。
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        # 创建一个外星人，并将其加入当前行。
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def run_game(self):
         """开始游戏的主循环"""
