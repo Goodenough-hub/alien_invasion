518f097473b3799aea2de8a3c8c0c3b4d64a9938
Goodenough <2323342501@qq.com>
2023-05-08 14:48:44 +0800

    chapter12 武装飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -20,6 +21,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()  # 创建用于存储子弹的编组
 
         # 设置背景色
         self.bg_color = (230, 230, 230)  # 浅灰色
@@ -29,7 +31,19 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹。"""
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除消失的子弹。
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
 
     def _check_events(self):
         """响应按键和鼠标事件。"""
@@ -38,7 +52,6 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
-
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
@@ -52,6 +65,14 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组bullets中。"""
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets .add(new_bullet)
 
     def _check_keyup_events(self, event):
         """响应松开。"""
@@ -66,6 +87,8 @@ class AlienInvasion:
         # 每次循环都重绘屏幕。
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # 让最近绘制的屏幕可见。
         pygame.display.flip()
--- settings.py
+++ settings.py
@@ -10,3 +10,10 @@ class Settings:
 
         # 飞船设置
         self.ship_speed = 1.5
+
+        # 子弹设置
+        self.bullet_speed = 1.0  # 子弹的速度比飞船稍低
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)  # 深灰色子弹
+        self.bullet_allowed = 3
