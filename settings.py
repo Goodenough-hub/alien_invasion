6a860ba6cc6fba30bf34c09f70b00b57b8f66462
Goodenough <2323342501@qq.com>
2023-05-08 13:45:29 +0800

    屏幕尺寸特定

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings:
     def __init__(self):
         """初始化游戏的设置。"""
         # 屏幕设置
-        self.screen_width = 600
-        self.screen_height = 400
+        self.screen_width = 1000
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
         # 飞船设置
