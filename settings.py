366afde87e4f3a200d0437d5fb9a5de8f4cea39e
Goodenough <2323342501@qq.com>
2023-05-09 13:52:13 +0800

    外星人来了

--- settings.py
+++ settings.py
@@ -10,10 +10,17 @@ class Settings:
 
         # 飞船设置
         self.ship_speed = 1.5
+        self.ship_limit = 3  # 一开始玩家拥有的飞船数量
 
         # 子弹设置
-        self.bullet_speed = 1.0  # 子弹的速度比飞船稍低
+        self.bullet_speed = 1.5  # 子弹的速度比飞船稍低
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)  # 深灰色子弹
         self.bullet_allowed = 3
+
+        # 外星人设置
+        self.alien_speed = 0.6
+        self.fleet_drop_speed = 8
+        # fleet_direction为1表示向右移，为-1表示向左移。
+        self.fleet_direction = 1
